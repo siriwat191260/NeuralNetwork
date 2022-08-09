8640078239914e787992f4cf487cac1f8c4a0dc5
Siriwat Songwattana <pan-191260@hotmail.com>
2022-08-10 00:59:02 +0700

    - finish NN1 NN2 - Now config main

--- src/NeuronNetwork1.java
+++ src/NeuronNetwork1.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class NeuronNetwork {
-    ArrayList<Double[]> train_dataset;
-    ArrayList<Double[]> train_dataset_desired;
-    ArrayList<Double[]> test_dataset;
-    ArrayList<Double[]> test_dataset_desired;
-    int Neural_Layer[] = {8,16,16,16,16,1};
+public class NeuronNetwork1 {
+    ArrayList<List<Double>> train_dataset_cross;
+    ArrayList<List<Double>> train_dataset_desire_cross;
+    ArrayList<List<Double>> test_dataset_cross;
+    ArrayList<List<Double>> test_dataset_desire_cross;
+    int Neural_Layer[] = {2,16,16,16,2};
+
     Double Node[][] = new Double[Neural_Layer.length][];
     Double Desire_Node[] = new Double[Neural_Layer[Neural_Layer.length-1]];
     Double Local_Gradient_Node[][] = new Double[Neural_Layer.length][];
@@ -16,14 +18,14 @@ public class NeuronNetwork {
     Double Max_Epoch = 1000.0;
     Double biases = 0.1;
     Double learning_rate = 0.01;
-    Double momentum_rate = 0.01;
+    Double momentum_rate = 0.3;
     Double error_output[] = new Double[Neural_Layer[Neural_Layer.length-1]];
 
-    public NeuronNetwork(ArrayList a, ArrayList b, ArrayList c, ArrayList d) {
-        train_dataset = a;
-        train_dataset_desired = b;
-        test_dataset = c;
-        test_dataset_desired = d;
+    public NeuronNetwork1(ArrayList a, ArrayList b, ArrayList c, ArrayList d) {
+        train_dataset_cross = a;
+        train_dataset_desire_cross = b;
+        test_dataset_cross = c;
+        test_dataset_desire_cross = d;
         //node and local gradient in each layer
         for (int i = 0; i < Neural_Layer.length; i++) {
             Node[i] = new Double[Neural_Layer[i]];
@@ -40,76 +42,154 @@ public class NeuronNetwork {
 
     public void train() {
         int n = 0;
-        double sse ;
-        double avg_error = 1000.0;
-        double e = 0.0;
         System.out.println("=======================================================================================");
         System.out.println("                                        Train                                        ");
         System.out.println("=======================================================================================");
         while (n < Max_Epoch && Error_Avg > Min_Error) {
-            double sum_error = 0.0;
-            for (int data = 0; data < train_dataset.size(); data++) {
-                int rand_dataset = (int) (Math.random() * train_dataset.size()-1); //random index data
+            Double[] d = new Double[2];
+            Double[] g = new Double[2];
+            double tp = 0.0;
+            double tn = 0.0;
+            double fp = 0.0;
+            double fn = 0.0;
+            double precision = 0.0;
+            double recall = 0.0;
+            double accuracy = 0.0;
+            for (int data = 0; data < train_dataset_cross.size(); data++) {
+                int rand_dataset = (int) (Math.random() * train_dataset_cross.size()); //random index data
                 for (int data_input = 0; data_input < Neural_Layer[0]; data_input++) { //add data to input Node
-                    Node[0][data_input] = train_dataset.get(rand_dataset)[data_input];
+                    Node[0][data_input] = train_dataset_cross.get(rand_dataset).get(data_input);
                 }
                 for (int data_input = 0;data_input < Neural_Layer[Neural_Layer.length-1];data_input++){
-                    Desire_Node[data_input] = train_dataset_desired.get(rand_dataset)[data_input]; // add data to desire Node
+                    Desire_Node[data_input] = train_dataset_desire_cross.get(rand_dataset).get(data_input); // add data to desire Node
                 }
                 forward_pass();
                 get_error();
                 backward_pass();
                 weight_new();
 
-                double d = train_dataset_desired.get(rand_dataset)[0] * 700.0;
-                double g = Node[Neural_Layer.length - 1][0] * 700;
-                e = d-g; // error
-               System.out.println("Desire : "+ d + "\t\t || Output : " + g + " \t|| Error : " + e);
+                for(int i=0;i<Neural_Layer[Neural_Layer.length-1];i++){
+                    d[i] = train_dataset_desire_cross.get(rand_dataset).get(i);
+                    g[i] = Node[Neural_Layer.length - 1][i];
+//                    System.out.println("["+i+"] => Desire : "+ d[i]);
+                }
+                if(Node[Neural_Layer.length-1][0]>Node[Neural_Layer.length-1][1]) {
+                    g[0] = 1.0;
+                    g[1] = 0.0;
+//                    System.out.println("Output : " + 1 + " " + 0);
+                }else{
+                    g[0] = 0.0;
+                    g[1] = 1.0;
+//                    System.out.println("Output : "+ 0 +" "+1);
+                }
 
-                sum_error += Math.pow(error_output[0],2.0); // sum of error^2
-
+                // predict 0 1  result 0 1 : class A => true A
+                if(g[0].equals(d[0]) && g[1].equals(d[1])){
+                    tp++;
+                }
+                // predict 1 0 result 1 0 : class B => true B
+                if(g[1].equals(d[1]) && g[0].equals(d[0])){
+                    tn++;
+                }
+                // predict 0 1 result 1 0 : class A => false A
+                if(g[1].equals(d[0]) && g[0].equals(d[1])){
+                    fp++;
+                }
+                // predict 1 0 result 0 1 : class B => false B
+                if(g[0].equals(d[1]) && g[1].equals(d[0])){
+                    fn++;
+                }
             }
-            sse = sum_error/2.0;
-            avg_error = sse / train_dataset.size(); //average error
+            precision = tp/(tp+fp);
+            recall = tp/(tp+fn);
+            accuracy = (tp+tn)/(tp+tn+fp+fn);
+            if(n == Max_Epoch-1) {
+                System.out.println("tp : " + tp);
+                System.out.println("tn : " + tn);
+                System.out.println("fp : " + fp);
+                System.out.println("fn : " + fn);
+                System.out.println("Precision : " + precision);
+                System.out.println("Recall : " + recall);
+                System.out.println("Accuracy : " + accuracy);
+            }
             n++;//next epoch
         }
 
-        System.out.println("=======================================================================================");
-        System.out.println("Average final: "+ avg_error);
-        System.out.println("=======================================================================================");
     }
 
     public void test(){
-        double sse ;
-        double avg_error = 0.0;
-        double sum_error = 0.0;
+        Double[] d = new Double[2];
+        Double[] g = new Double[2];
+        double tp = 0.0;
+        double tn = 0.0;
+        double fp = 0.0;
+        double fn = 0.0;
+        double precision = 0.0;
+        double recall = 0.0;
+        double accuracy = 0.0;
+        System.out.println("=======================================================================================");
         System.out.println("                                        Test                                        ");
         System.out.println("=======================================================================================");
-        for (int data = 0; data < test_dataset.size(); data++) {
+        for (int data = 0; data < test_dataset_cross.size(); data++) {
 
-            for (int data_input = 0; data_input < Neural_Layer[0]; data_input++) {
-                Node[0][data_input] = test_dataset. get(data)[data_input];
+            for (int data_input = 0; data_input < Neural_Layer[0]; data_input++) { //add data to input Node
+                Node[0][data_input] = test_dataset_cross.get(data).get(data_input);
             }
-
             for (int data_input = 0;data_input < Neural_Layer[Neural_Layer.length-1];data_input++){
-                Desire_Node[data_input] = test_dataset_desired.get(data)[data_input];
+                Desire_Node[data_input] = test_dataset_desire_cross.get(data).get(data_input); // add data to desire Node
             }
             forward_pass();
             get_error();
 
-            double d = test_dataset_desired.get(data)[0] * 700.0;
-            double g = Node[Neural_Layer.length - 1][0] * 700.0;
-            for(int i=0;i<Neural_Layer[Neural_Layer.length-1];i++) {
-                sum_error += Math.pow(error_output[i], 2.0);
+            for(int i=0;i<Neural_Layer[Neural_Layer.length-1];i++){
+                d[i] = test_dataset_desire_cross.get(data).get(i);
+                g[i] = Node[Neural_Layer.length - 1][i];
+//                    System.out.println("["+i+"] => Desire : "+ d[i]);
             }
-            System.out.println("Desire : "+ d + " || Output : " + g + " || Error : " + (d - g) +" || E_Output :" + error_output[0]*700);
+            if(Node[Neural_Layer.length-1][0]>Node[Neural_Layer.length-1][1]) {
+                g[0] = 1.0;
+                g[1] = 0.0;
+//                    System.out.println("Output : " + 1 + " " + 0);
+            }else{
+                g[0] = 0.0;
+                g[1] = 1.0;
+//                    System.out.println("Output : "+ 0 +" "+1);
+            }
+
+            // predict 0 1  result 0 1 : class A => true A
+            if(g[0].equals(d[0]) && g[1].equals(d[1])){
+                tp++;
+            }
+            // predict 1 0 result 1 0 : class B => true B
+            if(g[1].equals(d[1]) && g[0].equals(d[0])){
+                tn++;
+            }
+            // predict 0 1 result 1 0 : class A => false A
+            if(g[1].equals(d[0]) && g[0].equals(d[1])){
+                fp++;
+            }
+            // predict 1 0 result 0 1 : class B => false B
+            if(g[0].equals(d[1]) && g[1].equals(d[0])){
+                fn++;
+            }
         }
-        sse = sum_error/2.0; //sum of squared error
-        avg_error = sse / test_dataset.size(); //average error
-        System.out.println("=======================================================================================");
-        System.out.println("Average : "+ avg_error);
-        System.out.println("=======================================================================================");
+        precision = tp/(tp+fp);
+        recall = tp/(tp+fn);
+        accuracy = (tp+tn)/(tp+tn+fp+fn);
+
+            System.out.println("tp : " + tp);
+            System.out.println("tn : " + tn);
+            System.out.println("fp : " + fp);
+            System.out.println("fn : " + fn);
+            System.out.println("Precision : " + precision);
+            System.out.println("Recall : " + recall);
+            System.out.println("Accuracy : " + accuracy);
+
+
+
     }
+
+
 
     public void forward_pass() {
         for (int layer = 0; layer < Neural_Layer.length - 1; layer++) {  //loop for each layer
--- src/main.java
+++ src/main.java
@@ -3,24 +3,38 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
+        //problem 1
         ArrayList<Double[]> train_dataset = new ArrayList<>();
         ArrayList<Double[]> train_dataset_desired = new ArrayList<>();
         ArrayList<Double[]> test_dataset = new ArrayList<>();
         ArrayList<Double[]> test_dataset_desired = new ArrayList<>();
+        //problem 2
+        ArrayList<List<Double>> train_dataset_cross = new ArrayList<>();
+        ArrayList<List<Double>> train_dataset_desire_cross = new ArrayList<>();
 
+        ArrayList<List<Double>>[] test_dataset_cross = new ArrayList[10];
+        ArrayList<List<Double>>[] test_dataset_desire_cross = new ArrayList[10];
+
+
+
+
+        String cond = "";
+
+        cond = "cross";
+
+        // flood data
+        if(cond == "flood"){
         try {
             File flood_data = new File("src/Flood_dataset.txt");
-            Scanner myReader = new Scanner(flood_data);
+            Scanner myReader_flood = new Scanner(flood_data);
 
             int j = 0;
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
+            while (myReader_flood.hasNextLine()) {
+                String data = myReader_flood.nextLine();
                 String[] data_splited = data.split("\t");
                 int i = 0;
 
-
-
-                if (j%10 == 0) {
+                if (j%10 == 0) {    //Test
                     Double[] data_input = new Double[8];
                     Double[] data_desired = new Double[1];
 
@@ -34,7 +48,7 @@ public class main {
                             test_dataset_desired.add(data_desired);
                         }
                     }
-                }else{
+                }else{              //Train
                     Double[] data_input = new Double[8];
                     Double[] data_desired = new Double[1];
 
@@ -51,15 +65,103 @@ public class main {
                 }
                 j++;
             }
-
-            myReader.close();
-
+            myReader_flood.close();
             NeuronNetwork n = new NeuronNetwork(train_dataset,train_dataset_desired,test_dataset,test_dataset_desired);
             n.train();
+            n.test();
 
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        }
+
+
+        // cross data
+        if(cond == "cross"){
+            try{
+                File cross_data = new File("src/cross.txt");
+                Scanner myReader_cross = new Scanner(cross_data);
+
+                int line = 1;
+                while (myReader_cross.hasNextLine()) {
+                    String data = myReader_cross.nextLine();
+                    if (line % 3 == 0 || (line + 1) % 3 == 0) {
+                        String[] data_splited = data.split("\\s+");
+                        List<Double> temp = new LinkedList<>();
+                        for (String d : data_splited) {
+                                temp.add(Double.parseDouble(d));
+                        }
+                            if(line % 3 == 0){
+                                if(line%10 == 0){
+                                    test_dataset_desire_cross[0].add(temp);
+                                }if(line%10 == 1){
+                                    test_dataset_desire_cross[1].add(temp);
+                                }if(line%10 == 2){
+                                    test_dataset_desire_cross[2].add(temp);
+                                }if(line%10 == 3){
+                                    test_dataset_desire_cross[3].add(temp);
+                                }if(line%10 == 4){
+                                    test_dataset_desire_cross[4].add(temp);
+                                }if(line%10 == 5){
+                                    test_dataset_desire_cross[5].add(temp);
+                                }if(line%10 == 6){
+                                    test_dataset_desire_cross[6].add(temp);
+                                }if(line%10 == 7){
+                                    test_dataset_desire_cross[7].add(temp);
+                                }if(line%10 == 8){
+                                    test_dataset_desire_cross[8].add(temp);
+                                }if(line%10 == 9){
+                                    test_dataset_desire_cross[9].add(temp);
+                                }else {
+                                    train_dataset_desire_cross.add(temp);
+                                }
+                            }else if((line+1)%3==0){
+                                if((line+1)%10 == 0){
+                                    test_dataset_cross[0].add(temp);
+                                }if((line+1)%10 == 1){
+                                    test_dataset_cross[1].add(temp);
+                                }if((line+1)%10 == 2){
+                                    test_dataset_cross[2].add(temp);
+                                }if((line+1)%10 == 3){
+                                    test_dataset_cross[3].add(temp);
+                                }if((line+1)%10 == 4){
+                                    test_dataset_cross[4].add(temp);
+                                }if((line+1)%10 == 5){
+                                    test_dataset_cross[5].add(temp);
+                                }if((line+1)%10 == 6){
+                                    test_dataset_cross[6].add(temp);
+                                }if((line+1)%10 == 7){
+                                    test_dataset_cross[7].add(temp);
+                                }if((line+1)%10 == 8){
+                                    test_dataset_cross[8].add(temp);
+                                }if((line+1)%10 == 9){
+                                    test_dataset_cross[9].add(temp);
+                                }
+                                else{
+                                    train_dataset_cross.add(temp);
+                                }
+                            }
+                    }
+                    line++;
+                }
+                    myReader_cross.close();
+
+                    for(int i = 0;i<10;i++){
+                        NeuronNetwork1 n = new NeuronNetwork1(train_dataset_cross,train_dataset_desire_cross,test_dataset_cross[i],test_dataset_desire_cross[i]);
+                        n.train();
+                        n.test();
+                    }
+
+
+
+            }catch (FileNotFoundException e){
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+
+
+
     }
 }
